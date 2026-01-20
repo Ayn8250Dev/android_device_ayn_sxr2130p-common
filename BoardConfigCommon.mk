bbe68ae5e401e0768bc2606cec59ddb914ad49e0
AnierinB <anierin@evolution-x.org>
2026-01-20 02:26:51 -0800

    sxr2130p-common: Initial common tree skeleton
    Change-Id: I98d95e166d47031479099dfc2c097ce8b2cf7833
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/ayn/sxr2130p-common
+
+# Include the proprietary files BoardConfig.
+include vendor/ayn/sxr2130p-common/BoardConfigVendor.mk
