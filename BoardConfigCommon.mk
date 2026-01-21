85e5e4ff180f501ba5e5a9b72114dbc56ee60673
AnierinB <anierin@evolution-x.org>
2026-01-21 05:13:43 +0000

    sxr2130p-common: Initial vendor blobs
    Change-Id: I909d6b4d18328227e1f6812550cb69729bc0c771
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,6 +60,8 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := \
+    $(COMMON_PATH)/vintf/manifest_kona.xml
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
@@ -127,12 +129,23 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := kona
 
+# Properties
+PROPERTIES_PATH := $(COMMON_PATH)/properties
+TARGET_ODM_PROP += $(PROPERTIES_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(PROPERTIES_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(PROPERTIES_PATH)/system.prop
+TARGET_VENDOR_PROP += $(PROPERTIES_PATH)/vendor.prop
+
 # Recovery
 BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/init/fstab.default
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+
+# Security patch level
+BOOT_SECURITY_PATCH := 2024-01-01
+VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
